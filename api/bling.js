d219d1602564791551c06aca7e3be5d1de180ee9
ayyash <amala@sanadcash.com>
2024-10-05 20:31:34 +0300

    bling state

--- api/bling.js
+++ api/bling.js
@@ -10,31 +10,49 @@ module.exports = function () {
         res.json(o);
     });
 
+    router.post('/mscp', function (req, res) {
+        const o = require('../data/jobs.json').jobs;
+        const j = req.body;
+        j.id = uuid();
+        o.push(j);
+
+        res.json(j);
+    });
+    router.put('/mscp/:id', function (req, res) {
+        const o = require('../data/jobs.json').jobs;
+        const j = req.body;
+
+        const _j = o.find(j => j.id === req.params.id);
+        if (_j) {
+            Object.assign(_j, j);
+        }
+        res.json(_j);
+    });
+
 
     router.get('/mscp/:id', function (req, res) {
-        
-        
+
+
         const p = require('../data/details.json');
-        const _p = {...p};
+        const _p = { ...p };
         _p.mscp.name = req.params.id;
         res.json(_p);
     });
 
-    
+
     router.post('/mscp/:id/:action', function (req, res) {
-        
+        // /action is startTransfer stopTransfer
+        // /stop /stopTransfer /startScanner stopScanner
         res.json({
             "result": "Start scanning and transfer"
         });
     });
 
-    // get ingrdients
     router.get('/mscp/:id/state', function (req, res) {
         const p = require('../data/state.json')
         res.json(p);
     });
 
-    // get ingrdients
     router.get('/mscp/:id/summary', function (req, res) {
         const p = require('../data/summary.json')
         res.json(p);
